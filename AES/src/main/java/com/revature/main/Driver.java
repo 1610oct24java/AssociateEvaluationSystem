436849e4938d270394bca1a08935b5fb58d9d1e4
Travis Deshotels <travis.deshotels@gmail.com>
2016-12-21 19:09:55 -0500

    Add quote-escaped comma version.

--- AES/src/main/java/com/revature/main/Driver.java
+++ AES/src/main/java/com/revature/main/Driver.java
@@ -4,35 +4,28 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Driver {
-	public static void main(String[] args) {
-
+	private static final String commaReplacement = "!__comma__!";
+	
+	/*
+	 * Replace all instances of quoted commas with a placeholder.
+	 * This will enable splitting the file by commas.
+	 */	
+	private static List<String> escapeCommas(){
+		List<String> linesCleaned = new ArrayList<String>();
 		String csvFile = "input.csv";
 		BufferedReader br = null;
-		String line = "";
-		String cvsSplitBy = ",";
+		String lineInFile = "";
 
 		try {
 			br = new BufferedReader(new FileReader(csvFile));
-			// question line
-			while ((line = br.readLine()) != null) {
-
-				String[] lines = line.split(cvsSplitBy);
-				System.out.println("Question: " + lines[0].trim().replaceAll("`", ","));
-				System.out.println("Choices:");
-				for (int i = 1; i < lines.length; i++) {
-					System.out.println(lines[i].trim().replaceAll("`", ","));
-				}
-				// answer line
-				line = br.readLine();
-				lines = line.split(cvsSplitBy);
-				System.out.println("Answer: ");
-				for (int i = 0; i < lines.length; i++) {
-					System.out.println(lines[i].trim().replaceAll("`", ","));
-				}				
+			while ((lineInFile = br.readLine()) != null) {
+				lineInFile = lineInFile.replaceAll("\",\"", commaReplacement);
+				linesCleaned.add(lineInFile);
 			}
-
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -45,7 +38,39 @@ public class Driver {
 					e.printStackTrace();
 				}
 			}
-		}
+		}		
+		return linesCleaned;
+	}
+	
+	private static String placeCommas(String line){
+		return line.replaceAll(commaReplacement, ",");
+	}
+	
+	public static void main(String[] args) {
+		String cvsSplitBy = ",";
+		String[] linesList;
+		String line;
 
+		//have this return the list
+		List<String> linesCleaned = escapeCommas();
+		
+		for (int i=0;i<linesCleaned.size();i+=2){
+			// question line
+			line = linesCleaned.get(i);
+			linesList = line.split(cvsSplitBy);
+			System.out.println("Question:");
+			System.out.println(placeCommas(linesList[0].trim()));
+			System.out.println("Choices:");
+			for (int j=1;j<linesList.length;j++){
+				System.out.println(placeCommas(linesList[j].trim()));
+			}
+			// answer line
+			line = linesCleaned.get(i+1);
+			linesList = line.split(cvsSplitBy);
+			System.out.println("Answers:");
+			for (int j=0;j<linesList.length;j++){
+				System.out.println(placeCommas(linesList[j].trim()));
+			}
+		}
 	}
 }
