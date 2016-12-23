1f01807b0af3f76ae588daf1a9c564a0afc75ba6
WestonRWright <wrightw@uindy.edu>
2016-12-23 13:14:49 -0500

    updated Mailer and mailer properties

--- CoreInterface/src/main/java/com/revature/aes/beans/MailService.java
+++ CoreInterface/src/main/java/com/revature/aes/beans/MailService.java
@@ -6,63 +6,11 @@ import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
-/*  Anything of type MailSender is imported from springframework. Not to be 
- *  confused with mailService which is our service.
- */
-
 @Service("mailService")
 public class MailService {
 
 	@Autowired
 	private MailSender mailService;
-	private String recipient;
-	private String subject;
-	private String body;
-	private final String sender = "rev.thompson.noreply";
-	
-	MailService(){
-		super();
-	}
-	
-	MailService(String recipient, String subject, String body){
-		super();
-		this.recipient=recipient;
-		this.subject=subject;
-		this.body=body;
-	}
-	
-	public void setMailService(MailSender mailService)
-	{
-		this.mailService = mailService;
-	}
-	
-	public void setRecipient(String recipient) {
-		this.recipient = recipient;
-	}
-	
-	public String getRecipient(){
-		return this.recipient;
-	}
-	
-	public void setSubject(String subject){
-		this.subject = subject;
-	}
-	
-	public String getSubject(){
-		return this.subject;
-	}
-	
-	public void setBody(String body){
-		this.body = body;
-	}
-	
-	public String getBody(){
-		return this.body;
-	}
-	
-	public String getSender(){
-		return this.sender;
-	}
 	
 	public SimpleMailMessage setupMessage(String sender, String recipient, String sub, String body){
         SimpleMailMessage msg = new SimpleMailMessage();
