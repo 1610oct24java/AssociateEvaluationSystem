3769fda829ffb41977306a3939c586a17d55a4ac
Michelle Slay <chelle.slay.215@gmail.com>
2016-12-22 21:06:33 -0500

    TG-40 #testing

---
+++ CoreInterface/src/main/java/com/revature/aes/beans/UserImpl.java
@@ -0,0 +1,27 @@
+package com.revature.aes.beans;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("session")
+public class UserImpl implements User {
+	private String username;
+
+	@Override
+	public boolean checkUsername(String username) {
+		// 
+		
+		return this.username.equals(username);
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
+}
