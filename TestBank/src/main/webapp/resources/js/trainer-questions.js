714f5e7949c9a60a37a9e95cdd98ae3c79ebe90d
Edward Crader <Edward Crader>
2016-12-24 17:09:48 -0500

    added some angular enough for a base test of JNDI as well as full system architecture

--- TestBank/src/main/webapp/resources/js/trainer-questions.js
+++ TestBank/src/main/webapp/resources/js/trainer-questions.js
@@ -1,16 +1,55 @@
 'use strict';
 
-var app;
-var port = ":8090";
-var baseDirectory = "TestBank"
-var host = "http://192.168.0.15";
-var url = host + port + "/" + baseDirectory + "/"; 
-(() => {
-	app = angular.module('app', []);
-	app = angular.controller('FormatController', ['$scope', 'FeedService', ($scope, Feed) => {
-		$scope.loadFeeds = () => {
-			Feed.parseFeed(url + "format")
+var app; //the base application for angular.
+var port = ":8090"; //the port for ajax calls.
+var baseDirectory = "TestBank" //the base directory for AJAX calls.
+var domain = "http://192.168.0.15"; //the base domain for AJAX calls.
+var url = domain + port + "/" + baseDirectory + "/"; //a concatenation of the domain, port, and base directory to establish a base url.  
+
+/* A JavaScript closure of a function using ES2015 concise syntax.
+ * Using concise syntax of (()=> {})(); is equivalent of window.onload = function() {};
+ * This function uses Angular to control the overall functionality of the HTML page.  
+ */
+(() => 
+{
+	//creating the module for the base application whose name is 'app'
+	app = angular.module('app', []); 
+	
+	app = angular.controller('FormatController', ($http) =>
+	{
+		this.formatList = [];
+		this.format = {};
+		this.getFormatList = () =>
+		{
+			$http.get(url+ "format")
+				.then(response => 
+				{
+					this.formatList = response.data;
+				});
 		};
-		$scope.loadFeeds();
-	}]);
-})();
+		
+		this.getFormat = () => 
+		{
+			$http.get(url+"format/" +format)
+		};
+		
+		getFormatList();
+
+	});
+	
+	app = angular.controller('QuestionController', ($http)=>
+	{
+		this.questionList = [];
+		this.question = {};
+		this.getQuestionList = () => 
+		{
+			$http.get(url+"question")
+				.success((response)=>{
+					this.questionList = response.data;
+				});
+		}
+		
+		getQuestionList();
+	});
+})  //the end of the closure
+(); //invoking the function within the closure.
