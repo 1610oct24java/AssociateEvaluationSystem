dc3d075e01a231ff072abf219fbac7df997a7d23
Ryan Lawson <ryan.lawson2@gmail.com>
2016-12-27 14:56:55 -0500

    adding ability to execute snippets

--- Assessment/src/main/java/com/revature/aes/snippets/CompileSnippet.java
+++ Assessment/src/main/java/com/revature/aes/snippets/CompileSnippet.java
@@ -1,10 +1,8 @@
 package com.revature.aes.snippets;
 
-public class Compile {
+public class CompileSnippet {
 	public static boolean compileJava(String fullyQualifiedClassName) {
 		try {
-			Runtime.getRuntime().exec("javac " + fullyQualifiedClassName + ".java");
-			System.out.println("Compiling " + fullyQualifiedClassName);
 			Runtime.getRuntime().exec("java " + fullyQualifiedClassName);
 			System.out.println("Running " + fullyQualifiedClassName);
 			return true;
