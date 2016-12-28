72581969fa552d1d375dc6a9dfc95a9f999fecf2
Ryan Lawson <ryan.lawson2@gmail.com>
2016-12-27 22:30:36 -0500

    adding js for snippets

--- Assessment/src/main/java/com/revature/aes/snippets/IdeoneClient.java
+++ Assessment/src/main/java/com/revature/aes/snippets/IdeoneClient.java
@@ -1,25 +1,9 @@
 package com.revature.aes.snippets;
 
-import javax.xml.soap.SOAPConnection;
-import javax.xml.soap.SOAPConnectionFactory;
-import javax.xml.soap.SOAPException;
-
 public class IdeoneClient {
 	public static void main(String[] args){
-		SOAPConnectionFactory soapConnectionFactory = null;
 		try {
-			soapConnectionFactory = SOAPConnectionFactory.newInstance();
 		} catch (UnsupportedOperationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SOAPException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        try {
-			SOAPConnection soapConnection = soapConnectionFactory.createConnection();
-		} catch (SOAPException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
