e0ac5dc4ee4eafd53ec4ca499c4fba79dc6446d5
Christian Gonzalez <Christian Gonzalez>
2016-12-28 10:43:27 -0500

    Closer to making it work with Angular

--- Assessment/src/main/webapp/resources/js/app.js
+++ Assessment/src/main/webapp/resources/js/app.js
@@ -4,22 +4,40 @@ app.controller("quizController", function($scope, $http, $location) {
 	$scope.quiz = tstQuiz;
 	$scope.oneAtATime = false;
 	
-	$scope.editor = ace.edit("editor1");
+	$scope.editors = new Array();
+	
+	/*$scope.editor = ace.edit("editor1");
 	$scope.editor.setTheme("ace/theme/monokai");
-	$scope.editor.getSession().setMode("ace/mode/java");
+	$scope.editor.getSession().setMode("ace/mode/java");*/
 
-	var editor2 = ace.edit("editor2");
-	editor2.getSession().setMode("ace/mode/java");
+/*	var editor2 = ace.edit("editor2");
+	editor2.getSession().setMode("ace/mode/java");*/
 
 	$scope.test = function () {
 		console.log("Testing");
 		var out = $scope.editor.getValue();
 		console.log(out);
-	}
+	};
 	
 	$scope.status = {
 		isCustomHeaderOpen : false,
 		isFirstOpen : true,
 		isFirstDisabled : false
 	};
+	
+	$scope.checkNeedEditor = function (questionIndex) {
+		console.log("checkNeedEditor(" + questionIndex + ")");
+		var currQ = $scope.quiz.questions[questionIndex]
+		console.log(currQ.type);
+		
+		if (currQ.type === 3) {
+			// If this question is a coding question
+			console.log("Code question found");
+			//Create an editor for it
+			var newEditor = ace.edit("editor" + questionIndex);
+			newEditor.setTheme("ace/theme/monokai");
+			newEditor.getSession().setMode("ace/mode/java");
+			$scope.editors.push(newEditor);
+		}
+	}
 });
