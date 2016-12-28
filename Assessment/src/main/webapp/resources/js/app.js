54bbe69e242115afd042b9f5f6a651215820a301
Christian Gonzalez <Christian Gonzalez>
2016-12-27 21:48:58 -0500

    Minor format tweaks

--- Assessment/src/main/webapp/resources/js/app.js
+++ Assessment/src/main/webapp/resources/js/app.js
@@ -1,43 +1,8 @@
 var app = angular.module("quizApp", [ 'ui.bootstrap', 'as.sortable', 'ngAnimate' ]);
 
-app.controller('AccordionDemoCtrl', function ($scope) {
-  $scope.oneAtATime = true;
-
-  $scope.groups = [
-    {
-      title: 'Dynamic Group Header - 1',
-      content: 'Dynamic Group Body - 1'
-    },
-    {
-      title: 'Dynamic Group Header - 2',
-      content: 'Dynamic Group Body - 2'
-    }
-  ];
-
-  $scope.items = ['Item 1', 'Item 2', 'Item 3'];
-
-  $scope.addItem = function() {
-    var newItemNo = $scope.items.length + 1;
-    $scope.items.push('Item ' + newItemNo);
-  };
-
-  $scope.status = {
-    isCustomHeaderOpen: false,
-    isFirstOpen: true,
-    isFirstDisabled: false
-  };
-});
-
 app.controller("quizController", function($scope, $http, $location) {
 	$scope.quiz = tstQuiz;
-	$scope.oneAtATime = true;
-
-	$scope.items = [ 'Item 1', 'Item 2', 'Item 3' ];
-
-	$scope.addItem = function() {
-		var newItemNo = $scope.items.length + 1;
-		$scope.items.push('Item ' + newItemNo);
-	};
+	$scope.oneAtATime = false;
 
 	$scope.status = {
 		isCustomHeaderOpen : false,
