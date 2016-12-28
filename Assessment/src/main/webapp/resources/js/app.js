5757ab8f75260bf73909ea269e2f92785f4f0216
Christian Gonzalez <Christian Gonzalez>
2016-12-28 16:59:50 -0500

    Attempting to seperate controllers a bit.

--- Assessment/src/main/webapp/resources/js/app.js
+++ Assessment/src/main/webapp/resources/js/app.js
@@ -1,5 +1,22 @@
 var app = angular.module("quizApp", [ 'ui.bootstrap', 'as.sortable', 'ngAnimate' ]);
 
+app.controller("dragController"), function($scope) {
+	// DRAG AND DROP
+	$scope.dragControlListeners = {
+			accept: function (sourceItemHandleScope, destSortableScope) {return boolean},//override to determine drag is allowed or not. default is true.
+		    itemMoved: function (event) {},
+		    orderChanged: function(event) {}
+		    /*containment: '#board'//optional param.
+		    clone: true //optional param for clone feature.
+		    allowDuplicates: false //optional param allows duplicates to be dropped.*/
+	};
+
+	$scope.dragControlListeners1 = {
+			/*containment: '#board'//optional param.
+	        allowDuplicates: true //optional param allows duplicates to be dropped.*/
+	};
+}
+
 app.controller("quizController", function($scope, $http, $location) {
 	$scope.quiz = tstQuiz;
 	$scope.oneAtATime = false;
@@ -28,16 +45,9 @@ app.controller("quizController", function($scope, $http, $location) {
 	  };
 	  
 	  $scope.flagQuestion = function(index) {
-		  
 		  console.log("Entered flag: glyph id=" + index);
 		  $scope.states[index].flagged = !$scope.states[index].flagged;
 	  };
-
-	$scope.test = function () {
-		console.log("Testing");
-		var out = $scope.editors[3].getValue();
-		console.log(out);
-	};
 	
 	$scope.status = {
 		isCustomHeaderOpen : false,
@@ -45,28 +55,14 @@ app.controller("quizController", function($scope, $http, $location) {
 		isFirstDisabled : false
 	};
 	
-	$scope.dragControlListeners = {
-		    accept: function (sourceItemHandleScope, destSortableScope) {return boolean},//override to determine drag is allowed or not. default is true.
-		    itemMoved: function (event) {},
-		    orderChanged: function(event) {}
-		    /*containment: '#board'//optional param.
-		    clone: true //optional param for clone feature.
-		    allowDuplicates: false //optional param allows duplicates to be dropped.*/
-	};
-
-		$scope.dragControlListeners1 = {
-		        /*containment: '#board'//optional param.
-		        allowDuplicates: true //optional param allows duplicates to be dropped.
-*/		};
 	
+	
+	// EDITORS
 	$scope.checkNeedEditor = function (questionIndex) {
-		console.log("checkNeedEditor(" + questionIndex + ")");
 		var currQ = $scope.quiz.questions[questionIndex]
-		console.log(currQ.type);
 		
 		if (currQ.type === 3) {
 			// If this question is a coding question
-			console.log("Code question found");
 			//Create an editor for it
 			var newEditor = ace.edit("editor" + questionIndex);
 			newEditor.setTheme("ace/theme/monokai");
@@ -74,4 +70,31 @@ app.controller("quizController", function($scope, $http, $location) {
 			$scope.editors.push(newEditor);
 		};
 	};
+	
+	// PAGINATION
+	$scope.totalItems = 64;
+	$scope.currentPage = 4;
+	$scope.currQuestions;
+	
+	var getCurrQuestions = function(numPage) {
+		$scope.currQuestions = new Array();
+		for (var i = 0; i < 10; i++ ){
+			console.log("Push index: " + i + 10*(numPage-1));
+			$scope.currQuestions.push($scope.quiz.questions[i + 10*(numPage-1)]);
+		};
+	}
+	
+	$scope.setPage = function (pageNo) {
+	    $scope.currentPage = pageNo;
+	    
+	  };
+
+	  $scope.pageChanged = function() {
+	    $log.log('Page changed to: ' + $scope.currentPage);
+	    getCurrQuestions();
+	  };
+
+	  $scope.maxSize = 5;
+	  $scope.bigTotalItems = 175;
+	  $scope.bigCurrentPage = 1;
 });
--- Assessment/src/main/webapp/resources/js/testData.js
+++ Assessment/src/main/webapp/resources/js/testData.js
@@ -96,6 +96,102 @@ var tstQuiz = {
 					txtA : "private static void main(String[] pirateNoises){\n\n}"
 				}
 			]
+		},
+		{
+			txtQ : "Who was Anakin Skywalker's apprentice?",
+			id : 6,
+			type : 0,
+			options : [
+				{
+					txtA : "Luke Skywalker"
+				},
+				{
+					txtA : "Ahsoka Tano"
+				},
+				{
+					txtA : "Obi-Wan Kenobi"
+				}
+			]
+		},
+		{
+			txtQ : "At the end of Revenge of the Sith, which Jedi are confirmed to have survived Order 66?",
+			id : 7,
+			type : 1,
+			options : [
+				{
+					txtA : "Obi-Wan Kenobi"
+				},
+				{
+					txtA : "Mace Windu"
+				},
+				{
+					txtA : "The Younglings"
+				},
+				{
+					txtA : "Yoda"
+				},
+				{
+					txtA : "Kit Fisto"
+				}
+			]
+		},
+		{
+			txtQ : "Who shot first?",
+			id : 8,
+			type : 0,
+			options : [
+				{
+					txtA : "Han Solo"
+				},
+				{
+					txtA : "Greedo"
+				}
+			]
+		},
+		{
+			txtQ: "Write a program that prints 'May the Force be with you.' in Java",
+			id : 9,
+			type : 3,
+			options : [
+				{
+					txtA : "private static void main(String[] args){\n\n}"
+				}
+			]
+		},
+		{
+			txtQ : "What is the proper viewing order of these films?",
+			id: 10,
+			type : 2,
+			options : [
+				{
+					txtA : "Empire Strikes Back"
+				},
+				{
+					txtA : "The Phantom Menace"
+				},
+				{
+					txtA : "Return of The Jedi"
+				},
+				{
+					txtA : "Revenge of the Sith"
+				},
+				{
+					txtA : "Attack of the Clones"
+				},
+				{
+					txtA : "A New Hope"
+				},
+			]
+		},
+		{
+			txtQ : "Test",
+			id : 11,
+			type : 3,
+			options : [
+				{
+					txtA : "private static void main(String[] pirateNoises){\n\n}"
+				}
+			]
 		}
 	]
 };
