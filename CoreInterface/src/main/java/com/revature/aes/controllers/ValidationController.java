f906f17d38e62d16177f56f63d9fdc5cbc66c92f
Willie Jensen <Willie Jensen>
2016-12-29 15:07:51 -0500

    commit

--- CoreInterface/src/main/java/com/revature/aes/controllers/ValidationController.java
+++ CoreInterface/src/main/java/com/revature/aes/controllers/ValidationController.java
@@ -2,9 +2,11 @@ package com.revature.aes.controllers;
 
 import java.security.Principal;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class ValidationController {
 	
