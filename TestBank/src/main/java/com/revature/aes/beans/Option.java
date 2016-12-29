89079aa698a8706917323e6773b6186d211b0523
Leress <jdw751@msstate.edu>
2016-12-29 10:50:41 -0500

    Testing a refactoring techique
    test a way refactor

--- TestBank/src/main/java/com/revature/aes/beans/Option.java
+++ TestBank/src/main/java/com/revature/aes/beans/Option.java
@@ -113,10 +113,8 @@ public class Option implements Serializable {
 		
 		Option other = (Option) obj;
 		
-		if (correct == null) {
-			if (other.correct != null)
-				return false;
-		} else if (!correct.equals(other.correct))
+		if (((correct == null) & (other.correct != null)) || (!correct.equals(other.correct))) 
+			
 			return false;
 		
 		if (optionId == null) {
