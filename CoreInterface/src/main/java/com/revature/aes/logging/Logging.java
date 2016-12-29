c8e88338d31c0fe306f099d6269ef6e5aca8ceb6
WestonRWright <wrightw@uindy.edu>
2016-12-29 15:56:58 -0500

    smells

--- CoreInterface/src/main/java/com/revature/aes/logging/Logging.java
+++ CoreInterface/src/main/java/com/revature/aes/logging/Logging.java
@@ -15,7 +15,7 @@ public class Logging {
 	private String dashes = "\n==========================================================================================================================================================================";
 	
 	@Around("execution(* com.revature.aes..*(..))")
-	public Object log(ProceedingJoinPoint pjp){
+	public Object log(ProceedingJoinPoint pjp) throws Throwable{
 		MethodSignature signature = (MethodSignature) pjp.getSignature();
 		String methodClass = signature.getDeclaringTypeName().toString();
 		String method = signature.getName().toString();
@@ -33,7 +33,7 @@ public class Logging {
 		
 		try{
 			result = pjp.proceed();
-		} catch(Throwable e){
+		} catch(Exception e){
 			log.error("\t" + e.getClass() + " " + e.getMessage());
 			log.info(e);
 			
