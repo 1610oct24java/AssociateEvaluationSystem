c2892328043da456343c97899b02a2441d0811f2
Leress <jdw751@msstate.edu>
2016-12-29 03:10:12 -0500

    Reverted changes
    reverted changes to Option.java

--- TestBank/src/main/java/com/revature/aes/beans/Option.java
+++ TestBank/src/main/java/com/revature/aes/beans/Option.java
@@ -113,17 +113,20 @@ public class Option implements Serializable {
 		
 		Option other = (Option) obj;
 		
-		if ((correct == null) && (other.correct != null)) {
+		if (correct == null) {
+			if (other.correct != null)
 				return false;
 		} else if (!correct.equals(other.correct))
 			return false;
 		
-		if ((optionId == null) && (other.optionId != null)) {
+		if (optionId == null) {
+			if (other.optionId != null)
 				return false;
 		} else if (!optionId.equals(other.optionId))
 			return false;
 		
-		if ((optionText == null) && (other.optionText != null)) {
+		if (optionText == null) {
+			if (other.optionText != null)
 				return false;
 		} else if (!optionText.equals(other.optionText))
 			return false;
