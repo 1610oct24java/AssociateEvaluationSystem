766abe86c42ad964e7b0fa6343924e469cddc472
Christian Gonzalez <Christian Gonzalez>
2016-12-28 23:44:55 -0500

    Added pagination. Pagination works, broke some things in the process. I have a general idea how to fix them though.

--- Assessment/src/main/webapp/resources/js/app.js
+++ Assessment/src/main/webapp/resources/js/app.js
@@ -1,100 +1,103 @@
-var app = angular.module("quizApp", [ 'ui.bootstrap', 'as.sortable', 'ngAnimate' ]);
+var app = angular.module("quizApp", [ 'ui.bootstrap', 'as.sortable',
+		'ngAnimate' ]);
 
-app.controller("dragController"), function($scope) {
+app.controller("dragController", function($scope) {
 	// DRAG AND DROP
 	$scope.dragControlListeners = {
-			accept: function (sourceItemHandleScope, destSortableScope) {return boolean},//override to determine drag is allowed or not. default is true.
-		    itemMoved: function (event) {},
-		    orderChanged: function(event) {}
-		    /*containment: '#board'//optional param.
-		    clone: true //optional param for clone feature.
-		    allowDuplicates: false //optional param allows duplicates to be dropped.*/
+		accept : function(sourceItemHandleScope, destSortableScope) {
+			return boolean
+		},
+		itemMoved : function(event) {
+		},
+		orderChanged : function(event) {
+		}
 	};
 
 	$scope.dragControlListeners1 = {
-			/*containment: '#board'//optional param.
-	        allowDuplicates: true //optional param allows duplicates to be dropped.*/
 	};
-}
+});
 
 app.controller("quizController", function($scope, $http, $location) {
 	$scope.quiz = tstQuiz;
+	$scope.numEditors = 0;
 	$scope.oneAtATime = false;
 	$scope.states = new Array();
 	$scope.editors = new Array();
-	
+
+	var calcNumEditors = function() {
+		for (var i = 0; i < $scope.quiz.questions.length; i++) {
+			if ($scope.quiz.questions[i].type === 3) {
+				$scope.numEditors++;
+			}
+			;
+		}
+		;
+	};
+
+	calcNumEditors();
+
 	// Initialize states
-	for (var i=0; i < $scope.quiz.questions.length; i++)
-	  {
-		  var temp = {
-				  flagged : false,
-				  saved : false,
-				  open : true
-		  };
-		  $scope.states.push(temp);
-	  }
-	
-	 $scope.collapseQuestion = function(index) {
-		  console.log("Entered collapse: glyph id=" + index);
-		  $scope.states[index].open = !$scope.states[index].open;
-	  };
-	  
-	  $scope.saveQuestion = function(index) {
-		  console.log("Entered save: glyph id=" + index);
-		  $scope.states[index].saved = !$scope.states[index].saved;
-	  };
-	  
-	  $scope.flagQuestion = function(index) {
-		  console.log("Entered flag: glyph id=" + index);
-		  $scope.states[index].flagged = !$scope.states[index].flagged;
-	  };
-	
-	$scope.status = {
-		isCustomHeaderOpen : false,
-		isFirstOpen : true,
-		isFirstDisabled : false
-	};
-	
-	
-	
-	// EDITORS
-	$scope.checkNeedEditor = function (questionIndex) {
-		var currQ = $scope.quiz.questions[questionIndex]
-		
-		if (currQ.type === 3) {
-			// If this question is a coding question
-			//Create an editor for it
-			var newEditor = ace.edit("editor" + questionIndex);
-			newEditor.setTheme("ace/theme/monokai");
-			newEditor.getSession().setMode("ace/mode/" + "java");
-			$scope.editors.push(newEditor);
+	for (var i = 0; i < $scope.quiz.questions.length; i++) {
+		var temp = {
+			flagged : false,
+			saved : false,
+			open : false
 		};
-	};
-	
-	// PAGINATION
-	$scope.totalItems = 64;
-	$scope.currentPage = 4;
-	$scope.currQuestions;
-	
-	var getCurrQuestions = function(numPage) {
-		$scope.currQuestions = new Array();
-		for (var i = 0; i < 10; i++ ){
-			console.log("Push index: " + i + 10*(numPage-1));
-			$scope.currQuestions.push($scope.quiz.questions[i + 10*(numPage-1)]);
-		};
+		$scope.states.push(temp);
 	}
+	;
+
+	$scope.collapseQuestion = function(index) {
+		console.log("Entered collapse: glyph id=" + index);
+		$scope.states[index].open = !$scope.states[index].open;
+	};
+
+	$scope.saveQuestion = function(index) {
+		console.log("Entered save: glyph id=" + index);
+		$scope.states[index].saved = !$scope.states[index].saved;
+	};
+
+	$scope.flagQuestion = function(index) {
+		console.log("Entered flag: glyph id=" + index);
+		$scope.states[index].flagged = !$scope.states[index].flagged;
+	};
+
+	// EDITORS
+	$scope.checkNeedEditor = function(questionIndex) {
+		if ($scope.editors.length < $scope.numEditors) {
+			var currQ = $scope.quiz.questions[questionIndex];
+
+			if (currQ.type === 3) {
+				// If this question is a coding question
+				// Create an editor for it
+				var newEditor = ace.edit("editor" + questionIndex);
+				newEditor.setTheme("ace/theme/monokai");
+				newEditor.getSession().setMode("ace/mode/" + "java");
+				$scope.editors.push(newEditor);
+			}
+			;
+		}
+		;
+	};
+
+	/*
+	 * var initEditors = function () { for (var i = 0; i <
+	 * $scope.quiz.questions.length; i++) { console.log("Does " + i + " need an
+	 * editor?"); checkNeedEditor(i); }; };
+	 * 
+	 * initEditors();
+	 */
+
+	// PAGINATION
+	$scope.filteredQuestions = [];
+	$scope.currentPage = 1;
+	$scope.numPerPage = 3;
+	$scope.maxSize = 5;
 	
-	$scope.setPage = function (pageNo) {
-	    $scope.currentPage = pageNo;
-	    
-	  };
+	$scope.$watch('currentPage + numPerPage', function() {
+		var begin = (($scope.currentPage - 1) * $scope.numPerPage), end = begin
+				+ $scope.numPerPage;
 
-	  $scope.pageChanged = function() {
-	    $log.log('Page changed to: ' + $scope.currentPage);
-	    getCurrQuestions();
-	  };
-
-	  $scope.maxSize = 5;
-	  $scope.bigTotalItems = 175;
-	  $scope.bigCurrentPage = 1;
+		$scope.filteredQuestions = $scope.quiz.questions.slice(begin, end);
+	});
 });
