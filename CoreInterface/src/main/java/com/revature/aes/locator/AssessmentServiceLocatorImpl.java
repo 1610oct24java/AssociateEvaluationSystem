62acf7ba0b522cceb57515b3a2c45cb9431f0e55
Michelle Slay <parallelpandemonium@gmail.com>
2017-01-01 12:37:59 -0500

    code smells and reworking the logging a bit

--- CoreInterface/src/main/java/com/revature/aes/locator/AssessmentServiceLocatorImpl.java
+++ CoreInterface/src/main/java/com/revature/aes/locator/AssessmentServiceLocatorImpl.java
@@ -1,13 +1,28 @@
 package com.revature.aes.locator;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 import com.revature.aes.beans.AssessmentRequest;
 
 public class AssessmentServiceLocatorImpl implements AssessmentServiceLocator {
+	private RestTemplate restTemplate = new RestTemplate();
 
 	@Override
 	public AssessmentRequest getLink(AssessmentRequest request) {
-		// TODO Actually do everythign necessary for this to work
-		return null;
+		// TODO Change the URL to whatever Matthew's thingy will be
+		
+		String url = "http://www.google.com";
+		ResponseEntity<AssessmentRequest> responseEntity = restTemplate.postForEntity(url + "/assessment", request, AssessmentRequest.class);
+		
+		request = responseEntity.getBody();
+		HttpStatus httpStatus = responseEntity.getStatusCode();
+		
+		if(httpStatus != HttpStatus.OK)
+			return null;
+		
+		return request;
 	}
 
 }
