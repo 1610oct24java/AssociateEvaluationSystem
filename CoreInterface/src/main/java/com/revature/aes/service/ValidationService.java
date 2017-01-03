4c54020e8a75f133770d356f5de48d411ec753fa
HDXconnor <connor.odst@yahoo.com>
2017-01-03 13:14:29 -0500

    Removed all of the CoreInteface stuff that was added to the branch

--- CoreInterface/src/main/java/com/revature/aes/service/ValidationService.java
+++
@@ -1,9 +0,0 @@
-package com.revature.aes.service;
-
-import com.revature.aes.beans.User;
-
-public interface ValidationService {
-	public boolean validate(int userid);
-
-	public User register(int userid);
-}
