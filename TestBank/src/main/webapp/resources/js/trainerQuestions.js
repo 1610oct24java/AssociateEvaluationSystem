0fbeb33161c4f742305e05c54b476cc6c1757bda
HDXconnor <connor.odst@yahoo.com>
2017-01-03 11:06:17 -0500

    condensed header into externally included file

--- TestBank/src/main/webapp/resources/js/trainerQuestions.js
+++ TestBank/src/main/webapp/resources/js/trainerQuestions.js
@@ -1,125 +1,235 @@
-'use strict';
+	'use strict';
 
-var app; //the base application for angular.
-var port = ":8090"; //the port for ajax calls.
-var baseDirectory = "TestBank" //the base directory for AJAX calls.
-var domain = "http://localhost"; //the base domain for AJAX calls.
-var url = domain + port + "/" + baseDirectory + "/"; //a concatenation of the domain, port, and base directory to establish a base url.  
-var formatList = [];
-var questionformat = {};
-var questionList = [];
-var question = {};
-
-/* A JavaScript closure of a function using ES2015 concise syntax.
- * Using concise syntax of (()=> {})(); is equivalent of window.onload = function() {};
- * This function uses Angular to control the overall functionality of the HTML page.  
+var app; // the base application for angular.
+var port = ":8090"; // the port for ajax calls.
+var baseDirectory = "TestBank" // the base directory for AJAX calls.
+var domain = "http://localhost"; // the base domain for AJAX calls.
+var url =  /*domain + port + */"/" + baseDirectory + "/"; // a concatenation
+															// of
+														// the domain, port, and
+														// base directory to
+														// establish a base url.
+/*
+ * A JavaScript closure of a function using ES2015 concise syntax. Using concise
+ * syntax of (()=> {})(); is equivalent of window.onload = function() {}; This
+ * function uses Angular to control the overall functionality of the HTML page.
  */
-(() => 
-{
+(() => {
 	
-	//creating the module for the base application whose name is 'app'
-	app = angular.module('app', ['ui.bootstrap', 'ui.bootstrap.tpls']); 
-	
-    app.controller('FormatController', function($http)
-	{	
-    	this.fList;
-    	this.format;
-		this.getFormatList = () =>
-		{
-			$http.get(url+ "format")
-				.then(response => 
-				{
-					formatList = response.data;
-					this.fList = formatList;
-				});
-		};
+	// creating the module for the base application whose name is 'app'
+	app = angular.module('app', [ 'ui.bootstrap', 'ui.bootstrap.tpls' ]);
+
+	app.controller('FormatController', function($http) {
+		this.fList;
+		/*
+		 * var getFormatList = function() { $http.get(url + "format") .then(
+		 * function(response { formatList = response.data; this.fList =
+		 * formatList; });}
+		 */
+		this.getFormatList = () => {
+			$http.get(url + "format")
+			.then(response => {
+				this.fList = response.data;
+			});// $http end;
+		};// getFormatList() end
 		
-		this.getFormat = () => 
-		{
-			$http.get(url+"format/" +format)
-		};
-		
-		
-		
-		this.setFormat = () => {
-			questionformat = this.format;
-			console.log(questionformat)
-		};
-		angular.element(document).ready(()=> {
+		angular.element(document).ready(() => {
 			this.getFormatList();
-	    });
-	});
-	
-	app.controller('QuestionController', function($http)
-	{
-		this.format = {
-				format: 0,
-				formatName: ''
-		};
+		}); // angular element end
+	}); // FormatController end
+
+	app.controller('QuestionController', function($http) {
 		
-		this.question = {
-			questionId: 0,
-			questionText : '',
-			format: {
-				formatId: 0,
-				formatName: ''
-			}
-		};
+		this.formatSet = false;
+		this.questionTextChanged = false;
+		this.optionTextChanged = false;
+		this.correctValue = false;
+		this.addButton = false;
+		this.updatedQuestion;
+		this.show = false;
+		this.qList;
 		this.deleteme = 0;
-		this.getQuestionList = () => 
-		{	
-			$http.get(url+"question")
-				.then(response=>{
-					questionList = response.data;
-				});
+		this.selected = {};
+		this.format = {
+			format : 0,
+			formatName : ''
 		};
 		
-		this.addQuestion = () => 
-		{
+		this.option = {
+				optionId: 0,
+				optionText: '',
+				correct: -1
+		};
+		this.question = {
+			question: {
+				questionId : 0,
+				questionText : '',
+				format : {
+					formatId : 0,
+					formatName : ''
+				}
+			},
+			tags : null,
+			categories: null,
+			multiChoice:null,
+			dragDrops:null,
+			snippetTemplate:null
+		};
+		
+		// Retrieves the List of Questions from the Database
+		this.getQuestionList = () => {
+			$http.get(url + "question")
+				.then(response => {	
+					this.qList = response.data;
+				}); // $http end
+		}; // getQuestionList() end
+		
+		// Adds a option to a Question being created
+		this.addOption = () =>{
+			if(this.question.multiChoice == null){
+				this.question.multiChoice = [];
+			}// end if
+			if(this.option.optionText == ''){
+				alert("Please enter a Option");
+			} else if(this.option.correct == -1){
+				alert("Please Choose Yes or No for a Correct Answer");
+			} else {
+				this.question.multiChoice.push(this.option);
+				this.option = {
+						optionId: 0,
+						optionText: '',
+						correct: -1
+				};
+			};// end if
+		};
+		// This functions ensures a user populates all the necessary fields for
+		// a question.
+		this.addAddQuestionButton = (x) => {
+			switch(x) {
 			
-			this.question.format = questionformat;
-			console.log(this.question + ":::::");
-			console.log("adding question: " + this.question)
-			if(questionformat.formatId === 0 ){
-				alert("please choose a format type")
-			} else {$http.post(url+"question", this.question)
-				.success((response) => {
-					this.question = response.data;
-					if(question == null){
-						alert("Error Saving Question Please Try Again");
-					} else {
-						this.getQuestionList();
-						this.question ={
-								questionId: 0,
-								questionText : '',
-								format: {
-									formatId:0,
-									formatName: ''
-								}
-							};
-					};
-				});
-				
+			case 1:
+				this.formatSet = true;
+				break;
+			case 2:
+				this.questionTextChanged = true;
+				break;
+			case 3:
+				this.optionTextChanged = true;
+				break;
+			case 4:
+				this.correctValue = true;
+				break;
+			default:
+			} // switch end
+			
+			if(this.formatSet == true && this.questionTextChanged == true && this.optionTextChanged == true && this.correctValue == true){
+				this.addButton = true;
 			}
-			
-		};
+		}; // addAddQuestionButton end
 		
-		this.deleteQuestion = () => 
-		{
+		this.addQuestion = () => {
+			this.question.question.format = this.format;
+			if (this.question.question.format.formatId === 0) {
+				alert("please choose a format type");
+			} else {
+				if(this.question.questionText != ''){
+					console.log("Saving ");
+					console.log(this.question);
+				$http.post(url + "fullQuestion", this.question)
+					.success((response) => {
+						this.question = response.data;
+						if (this.question == null) {
+							alert("Error Saving Question Please Try Again");
+						} else {
+							this.getQuestionList();
+							this.question = {
+								question : {	
+									questionId : 0,
+									questionText : '',
+									format : {
+										formatId : 0,
+										formatName : ''
+									}
+								},
+									tags : null,
+									categories: null,
+									multiChoice:null,
+									dragDrops:null,
+									snippetTemplate:null
+								}; // this.question end
+							};// inner most if end
+						}); // $http end
+					} else {
+					alert("Please Enter A Question Text");
+				}// mid if end
+			}	// outer if end
+		}; // addQuestion() end
+
+		this.deleteQuestion = () => {
 			$http.delete(url + "question/" + this.deleteme)
 				.success((response) => {
 					this.getQuestionList();
 					alert("delete successful");
 				})
-				.error(()=>{
+				.error(() => {
 					alert("unable to properly delete the question");
-				});
-		};
-	
+				}); // $http end
+		}; // deleteQuestion end
 		
+		this.showUpdateQuestion = (aQuestion) => {
+			if(this.question == null){
+				this.show=false;
+			} else {
+				this.updatedQuestion = aQuestion;
+				this.show = true;
+			}	// if end
+		}; // showUpdateQuestion end
+
+		this.updateQuestion = () => {
+			this.question.question.format = questionformat;
+			if (questionformat.formatId === 0) {
+				alert("please choose a format type");
+			} else {
+				$http.put(url + "question", this.question)
+					.success((response) => {
+						this.question = response.data;
+						if (question == null) {
+							alert("Error Saving Question Please Try Again");
+						} else {
+							this.getQuestionList();
+							this.question = {
+									question : {
+										questionId : 0,
+										questionText : '',
+										format : {
+											formatId : 0,
+											formatName : ''
+										}
+									},
+									tags : null,
+									categories: null,
+									multiChoice:null,
+									dragDrops: null,
+									snippetTemplate:null
+								};	// this.question end
+							this.show = false;
+						} // inner if end
+					}); // $http end
+			}; // outer if end
+			
+			} // updateQuestion() end
 		angular.element(document).ready(() => {
 			this.getQuestionList();
-	    });
-	});
-})  //the end of the closure
-(); //invoking the function within the closure.
+		}); // angular.element end
+	}); // QuestionController end
+	
+	app.controller('CategoryController', function($http){
+			
+	}); // CategoryController end
+	app.controller('TagController', function($http){
+			
+	}); // TagController end
+	
+	
+})// the end of the closure
+(); // invoking the function within the closure.
