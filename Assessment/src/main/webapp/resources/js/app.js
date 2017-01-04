f27df19a0a5e4c2db0eb0e1ddb3c20b0fa7d73f2
Christian Gonzalez <Christian Gonzalez>
2017-01-04 15:16:16 -0500

    Integrated Ric's new features (Timer and Popup)

--- Assessment/src/main/webapp/resources/js/app.js
+++ Assessment/src/main/webapp/resources/js/app.js
@@ -225,3 +225,81 @@ function closeSideNav() {
     document.getElementById("sidenav").style.width = "0";
     document.getElementById("page-container").style.marginRight = "0";
 }
+
+app.controller('ModalController', function($scope, $interval, $http) {
+	
+	$scope.submitAssessment = function(){
+		answerData = {"id":1};
+		
+		postAssessment(answerData);
+	}
+	
+	function postAssessment(answerData){
+		$http({
+			method: 'POST',
+			url: '/Assessment/submitAssessment',
+			headers: {'Content-Type': 'application/json'},
+			data: answerData
+		})
+		.then(function(response) {
+			//First function handles success
+			console.log("Answers sent: " + response.data);
+		}, function(response) {
+			//Second function handles error
+			console.log("status code: " + response.status);
+		});
+	}
+});
+
+/* COUNTDOWN TIMER LOGIC */
+app.controller('CountdownController', function($scope, $rootScope, $interval) {
+	
+	var startTime = 120;
+	$scope.minutes = 0;
+	$scope.seconds = startTime;
+	$scope.barUpdate = getBarUpdate();
+	$scope.submitModal = document.getElementById("submitModal");
+	
+//    m = checkTime(m);
+//    s = checkTime(s);
+    
+	var timer = $interval(function(){
+		$scope.barUpdate = {width:getBarUpdate()+'%'};
+		
+		//WHEN THE TIMER REACHES ZERO,
+		//OPEN THE SUBMIT MODAL
+		if ($scope.minutes < 0 && $scope.seconds < 0)
+		{
+			submitAssessment();
+		}
+	}, 1000);
+	
+	function getBarUpdate() {
+		$scope.seconds = $scope.seconds - 1;
+		
+		if ($scope.seconds < 0)
+		{
+			$scope.seconds = 59;
+			$scope.minutes = $scope.minutes - 1;
+		}
+
+		console.log($scope.minutes + "m " + $scope.seconds + "s");
+		
+		if ($scope.minutes < 0) {
+			$interval.cancel(timer);
+			showSubmitModal();
+			console.log("time over");
+		}
+		
+		return ((($scope.minutes*60)+$scope.seconds) / startTime) * 100;
+	}
+	
+	function checkTime(i) {
+	    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+	    return i;
+	}
+	
+	function showSubmitModal() {
+		submitModal.style.display = "block";
+	}
+});
