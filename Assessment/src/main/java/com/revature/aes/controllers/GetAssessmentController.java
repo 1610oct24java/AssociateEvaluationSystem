607a752da40fdbf1dcba02548b4d7c726636c90e
licelotte08 <licelottef@gmail.com>
2017-01-04 22:36:48 -0500

    bootandController

---
+++ Assessment/src/main/java/com/revature/aes/controllers/GetAssessmentController.java
@@ -0,0 +1,43 @@
+package com.revature.aes.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.aes.beans.Assessment;
+import com.revature.aes.dao.AssessmentService;
+
+@RestController
+public class GetAssessmentController 
+{
+	//@Autowired
+	private AssessmentService service;
+	
+	//@Autowired
+	private HttpSession httpSession;
+		 
+	@RequestMapping(value="/link", method=RequestMethod.POST)
+	public String getAssessmentID(@RequestBody int assessmentId)
+	{
+		httpSession.setAttribute("assessmentId", assessmentId);
+		return "thisIsALink";
+	}
+	
+	@RequestMapping(value="/view/{id}", method=RequestMethod.POST)
+	public String getAssessment(@PathVariable int AssessmentId) throws JsonProcessingException
+	{
+		String JSONString;
+		ObjectMapper mapper = new ObjectMapper();
+		httpSession.getAttribute("assessmentId"); 
+		Assessment assessment = service.findAssesmentByAssessmentId(AssessmentId);
+		System.out.println(assessment);
+		JSONString = mapper.writeValueAsString(assessment);
+		return JSONString;
+	}
+}
--- Assessment/src/main/java/com/revature/aes/controllers/SubmitAssessmentController.java
+++ Assessment/src/main/java/com/revature/aes/controllers/SubmitAssessmentController.java
@@ -2,9 +2,6 @@ package com.revature.aes.controllers;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,54 +15,18 @@ import com.revature.aes.dao.AssessmentService;
 
 @Controller
 public class SubmitAssessmentController {
-	
-	// @Autowired
-	AssessmentService service;
-	
-	// method: receiveAssessmentID from Core()
-	// httpSession.save(ID);
-	//
-	// method: getAssessment()
-	// id = session.get(ID);
-	// questions.getDataById(id);
-	// return questionsJSON;
-	// }
-	private static String TBurl = "http://localhost:1993/TestBank/";
-	private static String Aurl = "http://localhost:1993/Assessment/";
-	
-	@RequestMapping(value = "/getAssessmentId", method = RequestMethod.GET)
-	public String getAssessment()
-			throws JsonParseException, JsonMappingException, IOException {
-		System.out.println("getAssessment");
-		int HashId = 5;
-		// GET ID, GENERTE HASH, RETURN URL WITH HASH
-		
-		return Aurl + HashId;/// ??
-	}
-	
-	@RequestMapping(value = "/getAssessmentData", method = RequestMethod.GET)
-	public String getAssessmentQuestions(HttpSession sessionObj)
-			throws JsonParseException, JsonMappingException, IOException {
-		
-		// GET LINK WITH HASH_ID AND TURN IT BACK TO ASSESMENT ID AND SAVE ITNTO
-		// THE SESSION??
-		int id = 0; // we need a hash for the id
-		String JSONString;
-		Assessment assessment = service.findAssesmentByAssessmentId(id);
-		
-		JSONString = assessment.toString();
-		
-		return JSONString;/// ??
-	}
-	
-	@RequestMapping(value = "/submitAssessment", method = RequestMethod.POST)
-	public String saveAssessmentAnswers(@RequestBody String JsonUserAnswers)
-			throws JsonParseException, JsonMappingException, IOException {
+
+	 //@Autowired
+	  AssessmentService service;
+	 
+	@RequestMapping(value="/submitAssessment",method = RequestMethod.POST)
+	public String saveAssessmentAnswers(@RequestBody String JsonUserAnswers) throws JsonParseException, JsonMappingException, IOException
+	{
 		ObjectMapper om = new ObjectMapper();
-		// SAVE the answers into the database
-		Assessment assessment = om.readValue(JsonUserAnswers, Assessment.class);
+		//SAVE the answers into the database
+		Assessment assessment = om.readValue(JsonUserAnswers,Assessment.class);
 		service.saveAssessmentByAssessment(assessment);
-		
-		return "redirect:pages/GoodBye.html";
+
+    	return "redirect:pages/GoodBye.html";
 	}
 }
