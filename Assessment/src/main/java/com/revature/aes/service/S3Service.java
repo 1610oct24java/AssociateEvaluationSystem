480ffbc1189bf12e396138a199b05c9e8ba52304
Ryan Lawson <ryan.lawson2@gmail.com>
2017-01-07 00:10:27 -0500

    modified s3service

--- Assessment/src/main/java/com/revature/aes/service/S3Service.java
+++ Assessment/src/main/java/com/revature/aes/service/S3Service.java
@@ -2,8 +2,8 @@ package com.revature.aes.service;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import org.springframework.stereotype.Service;
 
@@ -11,17 +11,15 @@ import com.revature.aes.io.SnippetIO;
 
 @Service
 public class S3Service {
-	
-	public boolean uploadToS3(String snippetContents, String key){
-		File file;
+
+	public boolean uploadToS3(String snippetContents, String key) {
+		File file = new File("tempFile");
 		try {
-			System.out.println("Upload contents: " + snippetContents);
-			file = File.createTempFile("snippet", ".tmp");
-			file.deleteOnExit();
-			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-			writer.write(snippetContents);
+		    BufferedWriter writer = new BufferedWriter(new PrintWriter(file));
+		    writer.write(snippetContents);
+		    writer.close();
 			new SnippetIO().upload(file, key);
-			writer.close();
+			file.delete();
 			return true;
 		} catch (IOException e) {
 			e.printStackTrace();
