221d0cdb9f547009c7e1c6cc9e8d2ea73351538f
Michelle Slay <parallelpandemonium@gmail.com>
2017-01-06 22:46:34 -0500

    fixing the fake assesment locator so it doesn't break spring

--- CoreInterface/src/main/java/com/revature/aes/locator/FAKEAssesmentServiceLocator.java
+++ CoreInterface/src/main/java/com/revature/aes/locator/FAKEAssesmentServiceLocator.java
@@ -1,10 +1,9 @@
 package com.revature.aes.locator;
 
 import org.apache.log4j.Logger;
-import org.springframework.stereotype.Service;
 
 import com.revature.aes.beans.AssessmentRequest;
-@Service
+
 public class FAKEAssesmentServiceLocator implements AssessmentServiceLocator {
 	Logger log = Logger.getRootLogger();
 	
