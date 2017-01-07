eebd5ea4902bbb5aca2749e42f39cd531bcf9f96
Leress <jdw751@msstate.edu>
2017-01-07 15:16:28 -0500

    Test fix assessment Gen Service
    -Assessment Gen Service.

--- TestBank/src/main/java/com/revature/aes/services/AssessmentGenService.java
+++ TestBank/src/main/java/com/revature/aes/services/AssessmentGenService.java
@@ -42,14 +42,16 @@ public class AssessmentGenService {
 		this.file = file;
 	}
 	
-	public void uploadAikenFile(){
+	public void uploadAikenFile() throws InvalidFileTypeException, AikenSyntaxException,IOException {
 		try {
 			aikenParser.parseFile(file);
 			// redundant exception invalid file type 
 		} catch (InvalidFileTypeException | AikenSyntaxException e) {
-			e.printStackTrace();
+			throw e;
+			
 		} catch (IOException e) {
-			e.printStackTrace();
+			throw e;
+			
 		}
 		HashMap<Question, ArrayList<Option>> questionsMap = (HashMap<Question, ArrayList<Option>>) aikenParser.getQuestionsMap();
 		
