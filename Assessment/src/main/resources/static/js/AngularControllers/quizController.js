954f9b7d60ee710243f1d2cfd612b2233627db85
Christian Gonzalez <Christian Gonzalez>
2017-01-08 20:23:24 -0500

    Fixed displaying multi-select

--- Assessment/src/main/resources/static/js/AngularControllers/quizController.js
+++ Assessment/src/main/resources/static/js/AngularControllers/quizController.js
@@ -65,7 +65,6 @@ app.controller("quizController", function($scope, $rootScope, $http) {
 		}
 	}
 
-
 	$scope.flagQuestion = function(index) {
 		$scope.states[index].flagged = !$scope.states[index].flagged;
 	};
