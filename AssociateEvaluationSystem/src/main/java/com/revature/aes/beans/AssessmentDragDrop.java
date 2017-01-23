57742fe68ff5c77b7cbe3a1b5f9d02d800b9a44f
Pski17 <mpski17@gmail.com>
2017-01-23 11:24:45 -0500

    saveAndFlush => save

--- AssociateEvaluationSystem/src/main/java/com/revature/aes/beans/AssessmentDragDrop.java
+++ AssociateEvaluationSystem/src/main/java/com/revature/aes/beans/AssessmentDragDrop.java
@@ -2,9 +2,15 @@ package com.revature.aes.beans;
 
 import java.io.Serializable;
 
-
-import javax.persistence.*;
-
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.revature.aes.service.AssessmentService;
--- AssociateEvaluationSystem/src/main/java/com/revature/aes/beans/SnippetTemplate.java
+++ AssociateEvaluationSystem/src/main/java/com/revature/aes/beans/SnippetTemplate.java
@@ -2,12 +2,21 @@ package com.revature.aes.beans;
 
 import java.io.Serializable;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Parameter;
 
 @Entity
 @Table(name="AES_SNIPPET_TEMPLATE")
