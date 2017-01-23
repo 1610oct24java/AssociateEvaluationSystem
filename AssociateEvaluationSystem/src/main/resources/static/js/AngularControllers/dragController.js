a8c54de67447656777dfc677c8d3e1d197439ec2
dataricc <rcsmith.dev@gmail.com>
2017-01-23 11:11:38 -0500

    fixed multiple choice selection bug

--- AssociateEvaluationSystem/src/main/resources/static/js/AngularControllers/dragController.js
+++ AssociateEvaluationSystem/src/main/resources/static/js/AngularControllers/dragController.js
@@ -9,27 +9,3 @@ app.controller("dragController", function($scope) {
 		}
 	};
 });
-
-app.controller('QuizNavController', function($scope, $rootScope) {
-	
-	$scope.index = 0;
-    $scope.array = [];
-    
-    $rootScope.initQuizNav = function () {
-    	for (var i=0; i < $rootScope.states.length/5; i++) {
-    		$scope.array.push(i);
-    	}
-    }
-    
-    $scope.$watch(function(){
-    	return $rootScope.states;
-    }, function() {
-		$scope.array = [];
-		
-		for(var i=0; i < $rootScope.states.length/5; i++){
-        	$scope.array.push(i);
-        }
-	});
-	
-	
-});
