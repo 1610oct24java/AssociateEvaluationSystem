151257b85e8546a0f331dbd62d9086e0a6d547d2
Corey <clshaps93@gmail.com>
2017-02-21 11:31:30 -0500

    First commit

--- automation-tests/nightwatch/tests/recruiter-login-1.js
+++ automation-tests/nightwatch/tests/recruiter-login-1.js
@@ -6,7 +6,7 @@ module.exports = {
       .url('http://35.162.177.133:8090/aes/login')
       .waitForElementVisible('body', 1000)
       .setValue('input[id=input-email]', 'nickolas.jurczak@revature.com')
-	  .setValue('input[id=input-password]', 'password')
+	    .setValue('input[id=input-password]', 'password')
       .waitForElementVisible('input[value=login]', 1000)
       .click('input[value=login]')
       .pause(3000)
