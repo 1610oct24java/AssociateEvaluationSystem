b11eb5f8fc0273ee3a3a6571dcf82420d4756e44
Matthew Ecker <eckermd@gmail.com>
2017-02-21 13:16:09 -0500

    Add user story 31

--- automation-tests/nightwatch/data/global_funcs.js
+++ automation-tests/nightwatch/data/global_funcs.js
@@ -8,5 +8,18 @@ module.exports = {
         .waitForElementVisible('input[value=login]', 1000)
         .click('input[value=login]')
         .pause(3000);
-    }
+    },
+	registerCandidate: function (browser, firstName, lastName, email) {
+		browser
+		  .click('a[href=recruit]')
+          .waitForElementVisible('input[id=inputFirstName]', 1000)
+	      .assert.title('AES | Register Candidate')
+		  .setValue('input[id=inputFirstName]', firstName)
+		  .setValue('input[id=inputLastName]', lastName)
+		  .setValue('input[id=inputEmail]', email)
+		  //.click('select[id=courseSelect]')
+          //.waitForElementVisible('option[value=option:3]', 1000)
+		  //.click('option[value=option:3]')
+		  //.click('input[id=btn-register-recruit]');
+	}
 };
