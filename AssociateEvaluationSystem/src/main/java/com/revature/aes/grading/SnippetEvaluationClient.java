31b24cdd4eee53bb672639e1b978348144481116
MykRod <michael.john.rod@gmail.com>
2017-03-01 13:40:19 -0500

    updating hulqBASH, adapting AES

--- AssociateEvaluationSystem/src/main/java/com/revature/aes/grading/SnippetEvaluationClient.java
+++ AssociateEvaluationSystem/src/main/java/com/revature/aes/grading/SnippetEvaluationClient.java
@@ -9,21 +9,22 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 
 public class SnippetEvaluationClient {
-	public int evaluateSnippet(String submissionKey, String solutionKey) {
+	public double evaluateSnippet(String submissionKey, String solutionKey) {
+		private static String hulqRESTlocation = "http://ec2-54-203-115-7.us-west-2.compute.amazonaws.com:8080";
 		HttpClient httpClient = HttpClientBuilder.create().build();
 		try {
-			HttpPost request = new HttpPost("http://ec2-54-203-115-7.us-west-2.compute.amazonaws.com:8080/FileRunner/submit");
-			StringEntity params = new StringEntity("{\"snippet\":\"" + submissionKey + "\",\"solution\":\"" + solutionKey + "\"} ");
+			HttpPost request = new HttpPost(hulqRESTlocation + "/submission);
+			StringEntity params = new StringEntity("{\"submission\":\"" + submissionKey + "\",\"solution\":\"" + solutionKey + "\"} ");
 			request.addHeader("content-type", "application/json");
 			request.setEntity(params);
 			HttpResponse response = httpClient.execute(request);
 			HttpEntity entity = response.getEntity();
 			String responseString = EntityUtils.toString(entity, "UTF-8");
-			System.out.println("Snippet: " + submissionKey + " [" + (Boolean.parseBoolean(responseString)? "pass" : "fail") + " ]");
-			return Boolean.parseBoolean(responseString) ? 1 : 0;
+			System.out.println("Source: " + submissionKey + " [grade: " + responseString + " ]");
+			return Double.parseDouble(responseString)
 
 		} catch (Exception e) {
-			return 1;
+			return 1.0;
 		}
 	}
 }
