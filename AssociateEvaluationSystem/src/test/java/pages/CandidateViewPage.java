6eb9ab38346b7dfe2bdbdc2ab7cd7946b9be4bc0
John Radcliffe <johnjradcliffe@gmail.com>
2017-03-09 16:41:47 -0500

    Worked on CandidateView POM

--- AssociateEvaluationSystem/src/test/java/pages/CandidateViewPage.java
+++ AssociateEvaluationSystem/src/test/java/pages/CandidateViewPage.java
@@ -1,7 +1,33 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class CandidateViewPage {
 
+	WebDriver driver;
+	
+	By registerCandidateLink = By.xpath("a[href=recruit]");
+	By logoutButton = By.id("logout");
+	
+	public CandidateViewPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public void clickRegisterCandidateLink() {
+		driver.findElement(registerCandidateLink).click();;
+	}
+	
+	public void clickLogoutButton() {
+		driver.findElement(logoutButton).click();
+	}
+	
+	public boolean verifyCandidate(String name, String email) {
+		boolean result = false;
+		
+		driver.findElement(By.xpath(""));
+		
+		return false;
+	}
 	
 }
