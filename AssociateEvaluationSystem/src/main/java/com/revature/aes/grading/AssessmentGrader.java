874f1eb2986646482373c76fabb7c5e70bbd180a
Lance Clauss <ldclauss2@gmail.com>
2017-03-09 14:17:03 -0500

    Pushed AssessmentGrader for testing

--- AssociateEvaluationSystem/src/main/java/com/revature/aes/grading/AssessmentGrader.java
+++ AssociateEvaluationSystem/src/main/java/com/revature/aes/grading/AssessmentGrader.java
@@ -226,6 +226,7 @@ public class AssessmentGrader {
 					//result[1] = result[1]+itemWeight;
 					log.info("out of " + countOptions + " options " + countCorrect +" were correct");
 				}
+				}
 			}
 			catch(ArithmeticException e){
 				log.stackTraceLogging(e);
