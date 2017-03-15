8882b038d3ac62a77392ecb01743121ace92b04e
Vincent <vinduong12@gmail.com>
2017-03-15 13:28:45 -0400

    hulqRest directory

--- hulqREST/src/main/java/com/revature/hulq/bash/BashException.java
+++ hulqREST/src/main/java/com/revature/hulq/bash/BashException.java
@@ -1,4 +1,4 @@
-package com.revature.hulq.exceptions;
+package com.revature.hulq.bash;
 
 public class BashException extends Exception {
 	private static final long serialVersionUID = -1098890947317892998L;
