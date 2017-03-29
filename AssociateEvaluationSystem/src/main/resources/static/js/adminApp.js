4d5e3891876b962e1144dbe3dfea97a7e0e69cf1
Nate Vardell <nvardell@gmail.com>
2017-03-29 14:39:25 -0400

    Fixed removing rows and display options for cats and formats

--- AssociateEvaluationSystem/src/main/resources/static/js/adminApp.js
+++ AssociateEvaluationSystem/src/main/resources/static/js/adminApp.js
@@ -276,7 +276,6 @@ adminApp.controller('UpdateEmployeeCtrl', function($scope,$location,$http,SITE_U
 
 adminApp.controller('CreateAssessmentCtrl', function($scope, $http, $mdToast, SITE_URL, API_URL, ROLE) {
 
-
     $scope.showToast = function(message, type) {
         $mdToast.show($mdToast.simple(message)
             .parent(document.querySelectorAll('#toastContainer'))
@@ -288,57 +287,113 @@ adminApp.controller('CreateAssessmentCtrl', function($scope, $http, $mdToast, SI
             .hideDelay(5000)
         );
     };
+    $scope.showToast("Success - Section added", "success");
 
+
+    /* Converts Current Sections Table into a 'DataTables' table */
     $(document).ready(function() {
         $('#example').DataTable({
             sDom: 'rt',
-            fixedColumns: true,
             scrollY: "220px",
             scrollX: false,
+            retrieve: true,
             paging: false
         });
     });
 
 
-    $http.get(SITE_URL.BASE + API_URL.BASE + API_URL.AUTH)
-        .then(function(response) {
-            if (response.data.authenticated) {
-                var authUser = {
-                    username: response.data.principal.username,
-                    authority: response.data.principal.authorities[0].authority
-                };
-                $scope.authUser = authUser;
-                if ($scope.authUser.authority != ROLE.ADMIN) {
-                    window.location = SITE_URL.LOGIN;
-                };
-            } else {
-                window.location = SITE_URL.LOGIN;
-            }
-        });
+    /* Quantity of questions per format/type */
+    var msQuestions = 0,
+        mcQuestions = 0,
+        ddQuestions = 0,
+        csQuestions = 0;
 
 
+    $scope.counts = [{ "category": "JavaScript", "formatType": "Multiple Choice", "total": 32 }, { "category": "OOP", "formatType": "Multiple Select", "total": 2 }, { "category": "C#", "formatType": "Multiple Select", "total": 8 }, { "category": "Java", "formatType": "Multiple Select", "total": 13 }, { "category": "CSS", "formatType": "Multiple Select", "total": 2 }, { "category": "Critical Thinking", "formatType": "Drag and Drop", "total": 1 }, { "category": "OOP", "formatType": "Multiple Choice", "total": 19 }, { "category": "C#", "formatType": "Multiple Choice", "total": 39 }, { "category": "HTML", "formatType": "Multiple Select", "total": 2 }, { "category": "Data Structures", "formatType": "Multiple Choice", "total": 6 }, { "category": "SQL", "formatType": "Drag and Drop", "total": 1 }, { "category": "SQL", "formatType": "Multiple Choice", "total": 17 }, { "category": "Data Structures", "formatType": "Multiple Select", "total": 2 }, { "category": "SQL", "formatType": "Multiple Select", "total": 4 }, { "category": "CSS", "formatType": "Multiple Choice", "total": 1 }, { "category": "Java", "formatType": "Multiple Choice", "total": 171 }, { "category": "JavaScript", "formatType": "Multiple Select", "total": 2 }, { "category": "Critical Thinking", "formatType": "Multiple Choice", "total": 13 }, { "category": "HTML", "formatType": "Multiple Choice", "total": 3 }, { "category": "Java", "formatType": "Code Snippet", "total": 9 }];
+    $scope.types = [{ "formatId": 4, "formatName": "Code Snippet" }, { "formatId": 3, "formatName": "Drag and Drop" }, { "formatId": 1, "formatName": "Multiple Choice" }, { "formatId": 2, "formatName": "Multiple Select" }];
+    $scope.categories = [{ "categoryId": 9, "name": "C#", "$$hashKey": "object:89" }, { "categoryId": 11, "name": "CSS", "$$hashKey": "object:90" }, { "categoryId": 5, "name": "Critical Thinking", "$$hashKey": "object:91" }, { "categoryId": 3, "name": "Data Structures", "$$hashKey": "object:92" }, { "categoryId": 10, "name": "HTML", "$$hashKey": "object:93" }, { "categoryId": 1, "name": "Java", "$$hashKey": "object:94" }, { "categoryId": 12, "name": "JavaScript", "$$hashKey": "object:95" }, { "categoryId": 2, "name": "OOP", "$$hashKey": "object:96" }, { "categoryId": 4, "name": "SQL", "$$hashKey": "object:97" }, { "categoryId": 6, "name": "core language", "$$hashKey": "object:98" }];
     $scope.times = [15, 30, 45, 60, 75, 90];
 
 
-    $scope.assessments = [];
+
+    /***********************************************************************************************************/
+    /***********************************************************************************************************/
+    /***********************************************************************************************************/
+    /***********************************************************************************************************/
+    /* Breaking down DB view in to special lists for some special shit!!! */
+    $scope.uniqueCats = NewUniqueArraybyId($scope.counts, "category");
+
+    function NewUniqueArraybyId(collection, keyname) {
+        var keys = [],
+            whatIWant = [];
+        var formatQuantities;
+
+        angular.forEach(collection, function(item) {
+            var key = item[keyname];
+            if (keys.indexOf(key) === -1) {
+                keys.push(key);
+            } else {
+                var index = keys.indexOf(key);
+            }
+        });
+
+        angular.forEach(keys, function(key) {
+            var myData = { "category": key, "formats": [] };
+            formatQuantities = { "name": "Multiple Choice", "type": "mcQuestions", "total": 0 };
+            myData.formats.push(formatQuantities);
+            formatQuantities = { "name": "Multiple Select", "type": "msQuestions", "total": 0 };
+            myData.formats.push(formatQuantities);
+            formatQuantities = { "name": "Drag and Drop", "type": "ddQuestions", "total": 0 };
+            myData.formats.push(formatQuantities);
+            formatQuantities = { "name": "Code Snippet", "type": "csQuestions", "total": 0 };
+            myData.formats.push(formatQuantities);
+            whatIWant.push(myData);
+        });
+
+        console.log(whatIWant[0]);
+
+        var test = keys.indexOf("OOP");
+        console.log(test);
+
+        return keys;
+    };
+
+    function getMaxMin() {
+        $scope.minQuantity = Math.min.apply(Math, $scope.counts.map(function(item) {
+            return item.total;
+        }));
+        console.log("Min = " + $scope.minQuantity);
+        $scope.maxQuantity = Math.max.apply(Math, $scope.counts.map(function(item) {
+            return item.total;
+        }));
+        console.log("Max = " + $scope.maxQuantity);
+    };
+
+
+    $scope.updateCatAndTypes = function() {
+
+    };
+    /***********************************************************************************************************/
+    /***********************************************************************************************************/
+    /***********************************************************************************************************/
+    /***********************************************************************************************************/
     $scope.sections = [];
+    $scope.assessments = [];
     $scope.totalQuestions = 0;
     $scope.totalCategories = 0;
     $scope.totalTypes = 0;
     $scope.time;
 
     function UpdateTotals(quantity) {
-        $scope.totalCategories = UniqueArraybyId($scope.assessments, "category");
-        console.log("Total categories = " + $scope.totalCategories);
-        $scope.totalTypes = UniqueArraybyId($scope.assessments, "type");
-        console.log("Total types = " + $scope.totalTypes);
+        $scope.totalCategories = UniqueArraybyId($scope.sections, "category");
+        $scope.totalTypes = UniqueArraybyId($scope.sections, "type");
         $scope.totalQuestions += quantity;
-        console.log("Total questions = " + $scope.totalQuestions);
-    }
+    };
 
 
     function UniqueArraybyId(collection, keyname) {
-        var output = [], keys = [];
+        var output = [],
+            keys = [];
 
         angular.forEach(collection, function(item) {
             var key = item[keyname];
@@ -351,72 +406,17 @@ adminApp.controller('CreateAssessmentCtrl', function($scope, $http, $mdToast, SI
     };
 
     $scope.removeRow = function(index) {
-        var tempQuantity = $scope.assessments[index]['quantity'];
-        $scope.assessments.splice(index, 1);
+        var tempQuantity = $scope.sections[index]['quantity'];
+        $scope.sections.splice(index, 1);
         UpdateTotals(-tempQuantity);
     };
 
     $scope.addRow = function() {
+        var tempCategory = $scope.categories[$scope.category];
+        $scope.sections.push({ 'category': tempCategory, 'type': $scope.type, 'quantity': $scope.quantity });
 
-        // $scope.temp = $scope.categories[$scope.categories.indexOf($scope.category.valueOf())];
-
-        $scope.sections.push({ 'category': $scope.category, 'type': $scope.type, 'quantity': $scope.quantity });
-
-
-        var tempCategory = $.grep($scope.categories, function(e){ return e.name == $scope.category; });
-
-
-        // console.log(result);
-        // console.log(result[0].categoryId);
-        // console.log(result[0].name);
-        // console.log($scope.category);
-
-        var msQuestions=0, mcQuestions=0, ddQuestions=0, csQuestions=0;
-
-        switch($scope.type) {
-            case $scope.types[0].formatName : { /* Multiple Choice */
-                console.log("case - 0");
-                mcQuestions = $scope.quantity;
-                console.log($scope.type); break;
-            }
-            case $scope.types[1].formatName : { /* Multiple Select */
-                console.log("case - 1");
-                msQuestions = $scope.quantity;
-                console.log($scope.type); break;
-            }
-            case $scope.types[2].formatName : { /* Drag 'n' Drop */
-                console.log("case - 2");
-                ddQuestions = $scope.quantity;
-                console.log($scope.type); break;
-            }
-            case $scope.types[3].formatName : { /* Code Snippet */
-                console.log("case - 3");
-                csQuestions = $scope.quantity;
-                console.log($scope.type); break;
-            }
-            default : {
-                console.log("case - default");
-                console.log($scope.type); break;
-            }
-        }
-
-
-
-        $scope.assessments.push({
-            "category": {
-                "categoryId": tempCategory[0].categoryId,
-                "name":  tempCategory[0].name
-            },
-            "msQuestions": msQuestions,
-            "mcQuestions": mcQuestions,
-            "ddQuestions": ddQuestions,
-            "csQuestions": csQuestions
-
-        });
-
-
-        console.log("Assessments - ");
-        console.log($scope.assessments);
+        console.log("Sections - Length = " + $scope.sections.length);
+        console.log($scope.sections);
 
 
         UpdateTotals($scope.quantity);
@@ -425,65 +425,58 @@ adminApp.controller('CreateAssessmentCtrl', function($scope, $http, $mdToast, SI
         $scope.category = '';
         $scope.type = '';
         $scope.quantity = '';
+
         $scope.showToast("Success - Section added", "success");
     };
 
-    //creates url and performs AJAX call to appropriate REST endpoint
-    //sending the assessment time and criteria
+
+    function createJSON() {
+        var data = [], json = { "timeLimit": $scope.time, "categoryRequestList": data };
+
+        angular.forEach($scope.sections, function(section) {
+            switch (section.type) {
+                case $scope.types[0].formatName: { /* Multiple Choice */
+                    mcQuestions = section.quantity;
+                    console.log("case - 0: " + section.type); break; }
+                case $scope.types[1].formatName: { /* Multiple Select */
+                    msQuestions = section.quantity;
+                    console.log("case - 1: " + section.type); break; }
+                case $scope.types[2].formatName: { /* Drag 'n' Drop */
+                    ddQuestions = section.quantity;
+                    console.log("case - 2: " + section.type); break; }
+                case $scope.types[3].formatName: { /* Code Snippet */
+                    csQuestions = section.quantity;
+                    console.log("case - 3: " + section.type); break; }
+                default: {
+                    console.log("case - default: " + section.type); break; }
+            }
+
+            data.push({
+                "category": { "categoryId": section.category.categoryId,"name": section.category.name },
+                "msQuestions": msQuestions,"mcQuestions": mcQuestions,
+                "ddQuestions": ddQuestions,"csQuestions": csQuestions
+            });
+
+
+            /* Reset question quantity per section */
+            msQuestions=0; mcQuestions=0; ddQuestions=0; csQuestions=0;
+        });
+
+        json.categoryRequestList = data;
+        console.log("This is my personal hand made JSON!!!!!");
+        console.log(json);
+        return json;
+    };
+
+
+    /* Performs AJAX call to REST endpoint sending assessment time and criteria */
     $scope.createAssessment = function() {
-
-        //build test JSON
-        // data = {
-        //     "timeLimit": $scope.time,
-        //     "categoryRequestList": [{
-        //         "category": {
-        //             "categoryId": 6,
-        //             "name": "core language"
-        //         },
-        //         "msQuestions": 5,
-        //         "mcQuestions": 25,
-        //         "ddQuestions": 0,
-        //         "csQuestions": 1
-        //     }, {
-        //         "category": {
-        //             "categoryId": 2,
-        //             "name": "OOP"
-        //         },
-        //         "msQuestions": 1,
-        //         "mcQuestions": 3,
-        //         "ddQuestions": 0,
-        //         "csQuestions": 0
-        //     }, {
-        //         "category": {
-        //             "categoryId": 3,
-        //             "name": "Data Structures"
-        //         },
-        //         "msQuestions": 1,
-        //         "mcQuestions": 0,
-        //         "ddQuestions": 0,
-        //         "csQuestions": 1
-        //     }, {
-        //         "category": {
-        //             "categoryId": 4,
-        //             "name": "SQL"
-        //         },
-        //         "msQuestions": 3
-        //     }]
-        // };
-
-        data = {
-            "timeLimit": $scope.time,
-            "categoryRequestList": $scope.assessments
-        };
-
-        //var sendUrl = SITE_URL.BASE + API_URL.BASE + "/assessmentrequest/" + "1";
-        // var sendUrl = SITE_URL.BASE + API_URL.BASE + "/assessmentrequest" + "/1/";
-
+        console.log("This is a masterpiece...");
         $http({
             method: 'PUT',
             url: (SITE_URL.BASE + API_URL.BASE + "/assessmentrequest" + "/1/"),
             headers: { 'Content-Type': 'application/json' },
-            data: data
+            data: (createJSON())
         }).success(function(response) {
             $scope.showToast("Assessment created successfully", "success");
             console.log("Assessment creation success");
@@ -492,40 +485,8 @@ adminApp.controller('CreateAssessmentCtrl', function($scope, $http, $mdToast, SI
             console.log("Assessment creation failed");
         });
     };
-
-
-
-
-    //initialize data
-    $http({
-        method: "GET",
-        url: "category"
-    }).then(function (response) {
-        $scope.categories = response.data;
-        console.log("Categories - ");
-        console.log(response);
-        $scope.showToast("Got all categories", "success");
-    });
-
-    $http({
-        method: "GET",
-        url: "format"
-    }).then(function (response) {
-        $scope.types = response.data;
-        console.log("Formats - ");
-        console.log(response);
-        $scope.showToast("Got all formats", "success");
-    });
-
-
-    //logout
-    $scope.logout = function() {
-        $http.post(SITE_URL.BASE + API_URL.BASE + API_URL.LOGOUT)
-            .then(function(response) {
-                window.location = SITE_URL.LOGIN;
-            })
-    };
 });
+
 
 adminApp.controller("menuCtrl", function($scope, $location, $timeout, $mdSidenav, $log) {
     var mc = this;
