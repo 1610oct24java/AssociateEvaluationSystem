6e94aed42eace2f6d477be732c8c2099a72ff102
tobon-marco <marco.tobon@email.ucr.edu>
2017-04-24 12:03:36 -0400

    === ADDED ECHOS FOR DEBUG ===

--- hulqREST/FizzBuzzIncorrectSolution.java
+++ hulqREST/FizzBuzzIncorrectSolution.java
@@ -1,10 +1,10 @@
 
-public class FizzBuzzIncorrectSolution {
+public class FizzBuzzAnswer {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println(args[0]+ "<-");
-        System.out.println(args[1]+ "<-");
+        //System.out.println(args[0]+ "<-");
+        ///System.out.println(args[1]+ "<-");
         for (int i = Integer.parseInt(args[0]); i < Integer.parseInt(args[1]); i++) {
 
             if (i % 3 == 0 && i % 5 == 0){
