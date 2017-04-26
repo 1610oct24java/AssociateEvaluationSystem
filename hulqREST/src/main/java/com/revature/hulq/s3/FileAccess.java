a0fe4a84b69d28e313b192d18402779faf2cdbdf
tobon-marco <marco.tobon@email.ucr.edu>
2017-04-26 11:03:04 -0400

    == S3LOCATION == Chagned location to test Assesment

--- hulqREST/src/main/java/com/revature/hulq/s3/FileAccess.java
+++ hulqREST/src/main/java/com/revature/hulq/s3/FileAccess.java
@@ -19,8 +19,8 @@ import com.amazonaws.services.s3.model.S3Object;
 
 public class FileAccess {
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
-	static private String S3LOCATION = "hulq-bash";
-//	static private String S3LOCATION = "aes.revature/";
+//	static private String S3LOCATION = "hulq-bash";
+	static private String S3LOCATION = "aes.revature/";
 
 	public boolean upload(String filename, String key){
         AmazonS3 s3client = new AmazonS3Client();
