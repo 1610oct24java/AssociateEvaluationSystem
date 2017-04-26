038f7687b338420765e5b216a7aedf2e1c792ff4
Xavier Grogan <xavier.grogan@aol.com>
2017-04-26 13:42:09 -0400

    fixing code smells in FileAccess.java

--- hulqREST/src/main/java/com/revature/hulq/s3/FileAccess.java
+++ hulqREST/src/main/java/com/revature/hulq/s3/FileAccess.java
@@ -19,109 +19,70 @@ import com.amazonaws.services.s3.model.S3Object;
 
 public class FileAccess {
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
-//	static private String S3LOCATION = "hulq-bash";
 	static private String S3LOCATION = "aes.revature/";
 
 	public boolean upload(String filename, String key){
         AmazonS3 s3client = new AmazonS3Client();
         try {
-            //System.out.println("Uploading a new object to S3 from a file\n");
         	log.info("Uploading a new object to S3 from a file\n");
             File file = new File(filename);
             s3client.putObject(new PutObjectRequest(S3LOCATION, key, file));
-            //System.out.println("Done.");
             log.info("Done.");
             return true;
          } catch (AmazonServiceException ase) {
-//            System.out.println("Caught an AmazonServiceException, which " +
-//            		"means your request made it " +
-//                    "to Amazon S3, but was rejected with an error response" +
-//                    " for some reason.");
             log.info("Caught an AmazonServiceException, which " +
             		"means your request made it " +
                     "to Amazon S3, but was rejected with an error response" +
                     " for some reason.");
-            //System.out.println("Error Message:    " + ase.getMessage());
             log.info("Error Message:     " + ase.getMessage());
-            //System.out.println("HTTP Status Code: " + ase.getStatusCode());
             log.info("HTTP Status Code:   " + ase.getStatusCode());
-            //System.out.println("AWS Error Code:   " + ase.getErrorCode());
             log.info("AWS Error Code:    " + ase.getErrorCode());
-            //System.out.println("Error Type:       " + ase.getErrorType());
             log.info("Error Type:         " + ase.getErrorType());
-            //System.out.println("Request ID:       " + ase.getRequestId());
             log.info("Request ID:         " + ase.getRequestId());
             return false;
         } catch (AmazonClientException ace) {
-//            System.out.println("Caught an AmazonClientException, which " +
-//            		"means the client encountered " +
-//                    "an internal error while trying to " +
-//                    "communicate with S3, " +
-//                    "such as not being able to access the network.");
             log.info("Caught an AmazonClientException, which " +
             		"means the client encountered " +
                     "an internal error while trying to " +
                     "communicate with S3, " +
                     "such as not being able to access the network.");
-            //System.out.println("Error Message: " + ace.getMessage());
             log.info("Error Message:  " + ace.getMessage());
             return false;
         }
     }
 	
 	public boolean download(String key){
-		//System.out.println("=== DOWNLOADING FILES FROM S3 ===");
 		log.info("======== DOWNLOADING FILES FROM S3 ========");
         AmazonS3 s3client = new AmazonS3Client();
         try {
-            //System.out.println("Downloading an object: " + key);
         	log.info("Downloading an object: " + key);
             File file=new File(key);
                       
             s3client.getObject(new GetObjectRequest(S3LOCATION, key),file);
             
-            //System.out.println("=== END DOWNLOADING FILES FROM S3 ===\n");
             log.info("====== END DOWNLOADING FILES FROM S3 ======\n");
         
             return true;
          } catch (AmazonServiceException ase) {
-//            System.out.println("Caught an AmazonServiceException, which " +
-//            		"means your request made it " +
-//                    "to Amazon S3, but was rejected with an error response" +
-//                    " for some reason.");
             log.info("Caught an AmazonServiceException, which " +
             		"means your request made it " +
                     "to Amazon S3, but was rejected with an error response" +
                     " for some reason.");
-            //System.out.println("Error Message:    " + ase.getMessage());
             log.info("Error Message:    " + ase.getMessage());
-            //System.out.println("HTTP Status Code: " + ase.getStatusCode());
             log.info("HTTP Status Code: " + ase.getStatusCode());
-            //System.out.println("AWS Error Code:   " + ase.getErrorCode());
             log.info("AWS Error Code:   " + ase.getErrorCode());
-            //System.out.println("Error Type:       " + ase.getErrorType());
             log.info("Error Type:       " + ase.getErrorType());
-            //System.out.println("Request ID:       " + ase.getRequestId());
             log.info("Request ID:       " + ase.getRequestId());
-            //System.out.println("Item:             " + key);
             log.info("Item:             " + key);
-            //System.out.println("=== END DOWNLOADING FILES FROM S3 (AMAZON SERVICE EXCEPTION) ===\n");
             log.info("====== END DOWNLOADING FILES FROM S3 (AMAZON SERVICE EXCEPTION) ======\n");
             return false;
         } catch (AmazonClientException ace) {
-//            System.out.println("Caught an AmazonClientException, which " +
-//            		"means the client encountered " +
-//                    "an internal error while trying to " +
-//                    "communicate with S3, " +
-//                    "such as not being able to access the network.");
         	log.info("Caught an AmazonClientException, which " +
             		"means the client encountered " +
                     "an internal error while trying to " +
                     "communicate with S3, " +
                     "such as not being able to access the network.");
-            //System.out.println("Error Message: " + ace.getMessage());
             log.info("Error Message: " + ace.getMessage());
-            //System.out.println("=== END DOWNLOADING FILES FROM S3 (AMAZON CLIENT EXCEPTION) ===\n");
             log.info("====== END DOWNLOADING FILES FROM S3 (AMAZON CLIENT EXCEPTION) ======\n");
             return false;
         }
