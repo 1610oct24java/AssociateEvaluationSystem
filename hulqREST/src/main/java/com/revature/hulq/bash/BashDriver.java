da3d785cf698992517b38b84eaeea4739b375228
tobon-marco <marco.tobon@email.ucr.edu>
2017-04-26 15:10:33 -0400

    Back To Normal

--- hulqREST/src/main/java/com/revature/hulq/bash/BashDriver.java
+++ hulqREST/src/main/java/com/revature/hulq/bash/BashDriver.java
@@ -313,28 +313,10 @@ public class BashDriver {
 		for (int i = 0; i <= key.length(); i++) {
 			matrix[i][0] = i;
 		}
-		//DEBUG PURPOSE PRINTING OUT MATRIX
-		log.info("----------------- AFTER KEY -------------------------");
-		for (int i = 0; i < matrix.length; i++) {
-		    for (int j = 0; j < matrix[i].length; j++) {
-		        System.out.print(matrix[i][j] + " ");
-		    }
-		    System.out.print("\n");
-		}
-		log.info("----------------- END AFTER KEY -------------------------");
 
 		for (int j = 0; j <= user.length(); j++) {
 			matrix[0][j] = j;
 		}
-
-		log.info("----------------- AFTER USER -------------------------");
-		for (int i = 0; i < matrix.length; i++) {
-		    for (int j = 0; j < matrix[i].length; j++) {
-		        System.out.print(matrix[i][j] + " ");
-		    }
-		    System.out.print("\n");
-		}
-		log.info("----------------- END USER KEY -------------------------");
 
 		int sub, add, del;
 		for (int i = 1; i <= key.length(); i++) {
@@ -349,15 +331,6 @@ public class BashDriver {
 				}
 			}
 		}
-		
-		log.info("----------------- AFTER ALL -------------------------");
-		for (int i = 0; i < matrix.length; i++) {
-		    for (int j = 0; j < matrix[i].length; j++) {
-		        System.out.print(matrix[i][j] + " ");
-		    }
-		    System.out.print("\n");
-		}
-		log.info("----------------- END AFTER KEY -------------------------");
 		
 		log.info("================ END stringCompare ==================");
 		return ((double)key.length() - (double)matrix[key.length() - 1][user.length() - 1])/(double)key.length();
