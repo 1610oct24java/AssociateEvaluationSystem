4d1534f125b4d3c7f61a05480b7d7fe64b8099af
keithminner <keith.minner14@gmail.com>
2017-04-28 12:42:02 -0400

    changing justification

--- AssociateEvaluationSystem/src/main/java/com/revature/aes/beans/Justification.java
+++ AssociateEvaluationSystem/src/main/java/com/revature/aes/beans/Justification.java
@@ -14,14 +14,6 @@ import org.hibernate.annotations.Parameter;
 public class Justification {
 	
 	@Id
-	@Column(name = "justification_id")
-	@GeneratedValue(generator = "AES_JUSTIFICATION_SEQ", strategy = GenerationType.SEQUENCE)
-	@GenericGenerator(name="AES_ASSESSMENT_SEQ", strategy="org.hibernate.id.enhanced.SequenceStyleGenerator", parameters={
-			@Parameter(name="sequence_name", value="AES_JUSTIFICATION_SEQ"),
-			@Parameter(name="optimizer", value="hilo"),
-			@Parameter(name="initial_value",value="1"),
-			@Parameter(name="increment_size",value="1")
-	})
 	private int justificationId;	
 	
 	@Column(name = "justification")
