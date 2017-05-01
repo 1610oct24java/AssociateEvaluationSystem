b07ef8ec7b7ef87f22b88189d7474ca89ea3dc1c
Aaron <Cammaaron@gmail.com>
2017-05-01 10:46:59 -0400

    can now set and save the settings

--- AssociateEvaluationSystem/src/main/resources/static/js/adminApp.js
+++ AssociateEvaluationSystem/src/main/resources/static/js/adminApp.js
@@ -1062,9 +1062,13 @@ adminApp.controller('ChooseAssessmentCtrl', function($scope, $http, SITE_URL, AP
 
 });
 
-adminApp.controller('SettingsViewCtrl', function($scope, $http, SITE_URL, API_URL, ROLE){
+adminApp.controller('SettingsViewCtrl', function($scope, $mdToast, $http, SITE_URL, API_URL, ROLE){
 	
 	var settingsUrl = SITE_URL.BASE + API_URL.BASE + API_URL.ADMIN + "/globalSettings"
+	
+	 $scope.showToast = function(message) {
+    	$mdToast.show($mdToast.simple().textContent(message).parent(document.querySelectorAll('#toastContainer')).position("center center").action("OKAY").highlightAction(true));
+    };
 	
 	$scope.getSettings = function(){
 		$scope.getSettingsUnsuccessful = false;
@@ -1076,8 +1080,10 @@ adminApp.controller('SettingsViewCtrl', function($scope, $http, SITE_URL, API_UR
                 $scope.getSettingsUnsuccessful = true;
             } else {
             	$scope.settings = {};
+            	$scope.settings.keys = [];
             	data.forEach(function (s){
             		$scope.settings[s.propertyId] = s;
+            		$scope.settings.keys.push(s.propertyId);
             	});
             }
 		}).error( function() {
@@ -1085,14 +1091,28 @@ adminApp.controller('SettingsViewCtrl', function($scope, $http, SITE_URL, API_UR
 		});
 	}
 	
-//	$scope.setSettings = function(){
-//		$http({
-//			method 	: 	'POST',
-//			url		:	settingsUrl,
-//			data	:	settings
-//		}).success(function)
-//		;
-//	}
+	$scope.setSettings = function(){
+		//turns the settings map back into a settings array
+		var settingsArray = [];
+		$scope.settings.keys.forEach(function (s){
+			settingsArray.push($scope.settings[s]); 
+		});
+		
+		$http({
+			method 	: 	'PUT',
+			url		:	settingsUrl,
+			data	:	settingsArray,
+			headers : {'Content-Type' : 'application/json'}
+		}).success(function (data){
+			if (data){
+				$scope.showToast("Settings Saved");
+			} else {
+				$scope.showToast("Settings Failed to Save");
+			}
+		}).error(function(){
+			$scope.showToast("Settings Failed to Save");
+		})
+	}
 	
 	$scope.getSettings();
 });
