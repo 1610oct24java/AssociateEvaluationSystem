917c0ca264b53fc8444dd449497869a2c2a20697
Xavier <xavier.grogan@aol.com>
2017-05-02 14:10:08 -0400

    smellfixes in Logging.java (#253)

--- hulqREST/src/main/java/com/revature/hulq/logging/Logging.java
+++ hulqREST/src/main/java/com/revature/hulq/logging/Logging.java
@@ -1,11 +1,6 @@
 package com.revature.hulq.logging;
 
-
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
