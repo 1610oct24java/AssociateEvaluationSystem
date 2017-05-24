e7e56e1acbe333ce1984a9c38bae2bea992ae783
Mehrab Rahman <mehrabmrahman@gmail.com>
2017-05-24 14:28:27 -0400

    first test framework

---
+++ AssociateEvaluationSystem/src/test/java/com/revature/aes/service/CustomUserDetailsServiceTest.java
@@ -0,0 +1,39 @@
+package com.revature.aes.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CustomUserDetailsServiceTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testLoadUserByUsername() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCheckForValidPassword() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetAuthorities() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetRoles() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetGrantedAuthorities() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
