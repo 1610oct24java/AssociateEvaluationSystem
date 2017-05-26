1f6891facc8d4bccddd81cdef18d80117ffc0d0a
richwingert <richwingert@gmail.com>
2017-05-26 15:03:07 -0400

    reorganized aadmin dashboard, some general UI changes

--- AssociateEvaluationSystem/src/main/resources/static/js/controllers/admin/adminDashboard.js
+++ AssociateEvaluationSystem/src/main/resources/static/js/controllers/admin/adminDashboard.js
@@ -82,7 +82,7 @@ adminApp.controller('AdminDashboardCtrl', function ($scope, $mdToast, $http, SIT
             var r = new rec($scope.recruiters[i].firstName, $scope.recruiters[i].lastName, $scope.recruiters[i].userId, 0 );
             $scope.recruiterCnts.push(r);
         }
-        var delr = new rec("Deleted","Recruiter", 0, 0 );
+        var delr = new rec("No Listed","Recruiter", 0, 0 );
         $scope.recruiterCnts.push(delr);
         //for each candidate, find their recruiter in our recruiter array
         //and increment the count for that recruiter
