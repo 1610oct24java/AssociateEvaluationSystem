1303c38a380640b04d19d673e32fc17e3ee08ee1
Mehrab Rahman <mehrabmrahman@gmail.com>
2017-05-30 10:51:04 -0400

    Architecture Controller Test & Bug Fixes (#307)
    * Added autogenerated documentation chm
    
    * CandidateCtrl re-refactoring
    
    * adding unit test for Rest controller CandidateController
    
    * More BugFixes
    
    * Fixed the older bugs and change ec2 to true
    
    * Refactor Security Tests
    
    * Fix merge conflict

--- AssociateEvaluationSystem/src/main/resources/static/js/controllers/bank/QuestionCtrl.js
+++ AssociateEvaluationSystem/src/main/resources/static/js/controllers/bank/QuestionCtrl.js
@@ -363,7 +363,7 @@ app.controller('QuestionCtrl', function($http, $scope, $filter) {
 
     $scope.addOption = function(newOption){
         newOption = {"optionText": newOption, "correct" : -1}
-        if(newOption != null && newOption != ""){
+        if(!newOption && newOption != ""){
             $http.post('question/addOption/'+ $scope.currentQuestion.questionId,JSON.stringify(newOption)).then(function(response){
                 window.response = response
                 console.log(response)
@@ -433,7 +433,7 @@ app.controller('QuestionCtrl', function($http, $scope, $filter) {
 
         if(document.getElementById("msrad").checked == false)
         {
-            document.getElementById("msrad").checked == true;
+        	document.getElementById("msrad").checked = true;
         }
     }
     $scope.setCat =function (category) {
