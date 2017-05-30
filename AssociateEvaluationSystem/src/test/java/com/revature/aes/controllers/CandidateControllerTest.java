e96ae119ba973d56ac9258f0de17faeca3a91e12
HackworthH <hackworth.ameliah@gmail.com>
2017-05-29 21:10:17 -0400

    adding working stand-alone unit test for Candidate rest controller

--- AssociateEvaluationSystem/src/test/java/com/revature/aes/controllers/CandidateControllerTest.java
+++ AssociateEvaluationSystem/src/test/java/com/revature/aes/controllers/CandidateControllerTest.java
@@ -1,12 +1,22 @@
 package com.revature.aes.controllers;
 
+/**
+ * Stand-alone Mockito JUnit test for the CandidateController (a Rest controller).
+ * Can be modified to add additional testing of features specific
+ * to this controller.  This test can be used as an example to set up testing
+ * for other Rest controllers.
+ * 
+ * @author Amelia Hackworth
+ * 
+ * reference: http://memorynotfound.com/unit-test-spring-mvc-rest-service-junit-mockito/
+ */
+
 
 import static org.mockito.Mockito.when;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-//import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,11 +24,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.hamcrest.core.Is.is;
 import com.revature.aes.beans.AssessmentAuth;
 import com.revature.aes.service.AssessmentAuthService;
 
@@ -27,17 +37,20 @@ public class CandidateControllerTest {
 	
 	private MockMvc mockMvc;
 	
+	//creates a mock of the AssessmentAuthService.
 	@Mock
 	private AssessmentAuthService authService;
 	
-	AssessmentAuth assessmentAuth = new AssessmentAuth(4350,260,"http://10.0.0.225:8090/aes","http://192.168.60.108:8090/aes/quiz?asmt=5Aq");
+	AssessmentAuth assessmentAuth = new AssessmentAuth(2152,136,"http://192.168.60.191:8090/aes","http://192.168.60.85:8090/aes/quiz?asmt=319");
 	
+	//input for the rest controllers' @PathVariable
 	String email = "ldclauss@svsu.edu";
 	
+	//injects the AssessmentAuthService into the candidate controller.
 	@InjectMocks
 	private CandidateController candidateController;
 	
-
+	//sets up the stand-alone testing for the CandidateController.
 	@Before
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
@@ -46,16 +59,14 @@ public class CandidateControllerTest {
 				.build();
 	}
 
+	//tests the response status and content type of the getLink method.  
 	@Test
 	public final void testGetLink() throws Exception {
 		
 		when(authService.getLink(Mockito.anyString())).thenReturn(assessmentAuth);
 		mockMvc.perform(get("/candidate/{email}/link", email))
 			.andExpect(status().isOk())
-			.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-			.andExpect(null);
+			.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
 	}
 	
-	
-
 }
