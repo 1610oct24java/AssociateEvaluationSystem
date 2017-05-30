31e55e288604b2f6a8ad7ef705116b931fee356e
Mehrab Rahman <mehrabmrahman@gmail.com>
2017-05-30 15:02:34 -0400

    vulnerabilities

--- AssociateEvaluationSystem/src/main/resources/static/js/controllers/admin/CreateAssessmentCtrl.js
+++ AssociateEvaluationSystem/src/main/resources/static/js/controllers/admin/CreateAssessmentCtrl.js
@@ -358,7 +358,7 @@ adminApp.controller('CreateAssessmentCtrl', function($scope, $http, $mdToast, $l
             $scope.numOfQuestions = response.data;
             $scope.quantity=$scope.maxQuestionsInput;
             if($scope.quantity > $scope.numOfQuestions){
-                alert("There are only " + $scope.numOfQuestions + " of those questions available.");
+                //alert("There are only " + $scope.numOfQuestions + " of those questions available.");
             }else{
                 $scope.sections.push({ 'category': $scope.currentCategory.name, 'type': $scope.currentType.formatName, 'quantity': $scope.quantity });
 
